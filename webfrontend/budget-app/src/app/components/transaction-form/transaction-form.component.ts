e43bc41e6ca6cfbb1ffcc59fc011131a7fdbdc85
Daniel Van Acker <danvan4477@gmail.com>
2020-08-01 01:34:26 -0400

    Update transaction-form.component.ts

--- webfrontend/budget-app/src/app/components/transaction-form/transaction-form.component.ts
+++ webfrontend/budget-app/src/app/components/transaction-form/transaction-form.component.ts
@@ -1,19 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
-export const HEROES: any[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
 @Component({
   selector: 'transaction-form',
   templateUrl: './transaction-form.component.html',
